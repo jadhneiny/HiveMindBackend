80f5e89624a42307b2d0528c8a63223d8da0c2b3
jadhneiny <jadhneiny123@gmail.com>
2024-12-05 19:20:34 +0200

    fixed bug

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Text, DateTime, func
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Text, DateTime, func
 from sqlalchemy.orm import relationship
 from database import Base
 
@@ -24,10 +24,11 @@ class Message(Base):
     chat = relationship("Chat")
     sender = relationship("User")
 
+
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
+    username = Column(String, nullable=False, unique=True)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
     is_tutor = Column(Boolean, default=False)
